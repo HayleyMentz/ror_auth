fd982ab86d6b3a637a77907732338983feb06cdf
Hayley Mentz <hayley.mentz@gmail.com>
2022-07-15 14:14:44 -0700

    auth done

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,24 @@ import Nomatch from './components/shared/Nomatch';
 import Navbar from '/components/shared/Navbar';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import FetchUser from './components/auth/FetchUser';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 
 const App = () => (
 <>
+<NavBar />
+<FetchUser>
 <Routes>
 <Route path ='/' element={<Home/>} />
+<Route path ='/' element={<ProtectedRoute/>} />
 
+</Route> 
+
+<Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/*' element={<Nomatch />} />
 </Routes>
-
+</FetchUser>
 </>
 
 )
