0239253a95cfa63e8131f2da1fb0f876210fdc91
Hayley Mentz <hayley.mentz@gmail.com>
2022-07-18 16:46:36 -0700

    seed file done

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_232648) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_18_232355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cats", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.string "registry"
+    t.string "avatar"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cats_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -44,4 +55,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_14_232648) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "cats", "users"
 end
