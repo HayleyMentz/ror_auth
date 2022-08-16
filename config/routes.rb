09f8a42183a2e6e331e068e673bd8fa4984167f8
Hayley Mentz <hayley.mentz@gmail.com>
2022-08-16 11:40:23 -0700

    added animation

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'api/user'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount_devise_token_auth_for 'User', at: 'api/auth'
+  
+  namespace :api do 
+    resources :users, only: :update
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  namespace :api do
     resources :cats do
-    resources ;notes
+      resources :notes 
     end
-
 
   end
 end
+
+
